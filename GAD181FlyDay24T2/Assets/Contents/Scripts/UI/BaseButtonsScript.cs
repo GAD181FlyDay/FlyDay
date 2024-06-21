b935b34f4be6896ab535543eaeba2a785cfca0d5
Dana <tetekim31@gmail.com>
2024-06-21 11:05:58 +0400

    Added folders and a blank script

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/UI/BaseButtonsScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+
+
+    public class BaseButtonsScript : MonoBehaviour
+    {
+
+        #region Play Button
+
+        #endregion
+        #region options Button
+
+        #endregion
+        #region Play Button
+
+        #endregion
+        #region Play Button
+
+        #endregion
+    }
+}

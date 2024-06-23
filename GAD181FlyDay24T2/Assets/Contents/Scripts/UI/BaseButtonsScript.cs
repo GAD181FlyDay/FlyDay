981e951085511a16ef3056583783beb829d2a67d
Dana <tetekim31@gmail.com>
2024-06-23 22:56:48 +0400

    Play button is functional.

--- GAD181FlyDay24T2/Assets/Contents/Scripts/UI/BaseButtonsScript.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/UI/BaseButtonsScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace UI
 {
@@ -9,16 +10,25 @@ namespace UI
     public class BaseButtonsScript : MonoBehaviour
     {
 
-        #region Play Button
+        #region Play Button 
+        /// <summary>
+        /// This function is assigned to the play button, 
+        /// it directs the player to the start of the game.
+        /// </summary>
+        public void StartTheGame()
+        {
+            // Loads the game scene.
+            SceneManager.LoadScene("MainGameScene");
+        }
 
         #endregion
         #region options Button
 
         #endregion
-        #region Play Button
+        #region Credits Button
 
         #endregion
-        #region Play Button
+        #region EXit Button
 
         #endregion
     }

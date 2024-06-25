becb3734e39c4902ed476aba47085f20e66a52fb
Dana <tetekim31@gmail.com>
2024-06-25 19:52:25 +0400

    Fixed Naming convention in the folders and script

--- GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController1 : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Animator playerAnimator;
     public Rigidbody playerRigidbody;
-    public float dSpeed, rotateSpeed;
-    public 
+    public float dSpeed, rotateSpeed; 
    
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");

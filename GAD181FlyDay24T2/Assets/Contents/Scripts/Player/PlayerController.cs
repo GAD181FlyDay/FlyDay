d0b6b2c82869d7776f889b83ba2f0f9d1c7b45a2
Dana <tetekim31@gmail.com>
2024-06-26 08:42:14 +0400

    Moved a script location and added a namespace to the first player controller

--- GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
@@ -2,56 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace PlayerOne
 {
-    public Animator playerAnimator;
-    public Rigidbody playerRigidbody;
-    public float dSpeed, rotateSpeed; 
-   
 
-    
-    void Start()
+
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        public Animator playerAnimator;
+        public Rigidbody playerRigidbody;
+        public float dSpeed, rotateSpeed;
 
-    
-    void FixedUpdate()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
 
-        if (movement == Vector3.zero)
+        void Start()
         {
-            return;
+
         }
 
-        playerRigidbody.MovePosition(playerRigidbody.position + movement * Time.fixedDeltaTime);
-    }
 
-    private void Update()
-    {
-        bool pressSneak = (Input.GetKeyDown(KeyCode.C));
-        bool pressWalk = (Input.GetKeyDown(KeyCode.D));
-        bool pressRunning = (Input.GetKeyDown(KeyCode.D));
-        bool running = playerAnimator.GetBool("Running");
-        bool walking = playerAnimator.GetBool("Walking");
-        bool sneaking = playerAnimator.GetBool("Sneaking");
-
-
-        if (Input.GetKeyDown(KeyCode.D))
+        void FixedUpdate()
         {
-            playerAnimator.SetBool("Walking", true);
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+
+            if (movement == Vector3.zero)
+            {
+                return;
+            }
+
+            playerRigidbody.MovePosition(playerRigidbody.position + movement * Time.fixedDeltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerAnimator.SetBool("Jumping", true);
-        }
-        if (Input.GetKeyDown(KeyCode.C))
-        {
 
+        private void Update()
+        {
+            bool pressSneak = (Input.GetKeyDown(KeyCode.C));
+            bool pressWalk = (Input.GetKeyDown(KeyCode.D));
+            bool pressRunning = (Input.GetKeyDown(KeyCode.D));
+            bool running = playerAnimator.GetBool("Running");
+            bool walking = playerAnimator.GetBool("Walking");
+            bool sneaking = playerAnimator.GetBool("Sneaking");
+
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                playerAnimator.SetBool("Walking", true);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerAnimator.SetBool("Jumping", true);
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+
+            }
         }
     }
 }

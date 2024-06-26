1b8bd334b0bae5810ab4d4545a545c141abb3205
Dana <tetekim31@gmail.com>
2024-06-26 08:32:58 +0400

    Player movement fixed
    Just changed horixontal to vertical in the player controller script

--- GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
 

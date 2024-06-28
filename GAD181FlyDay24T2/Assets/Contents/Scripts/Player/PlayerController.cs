458007af3230226bea961b3f7ac8034f3fd0fd54
Exkalybur9134 <kalyamba.mhango@student.saedubai.com>
2024-06-28 10:25:57 +0400

    Finally almost fully complete movement
    No falling over, rotation implemented on all accounts, Animation triggered to stop and start on command. Animation transitions complete. Still Need to add full jump

--- GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerController.cs
@@ -26,6 +26,7 @@ namespace PlayerOne
             float vertical = Input.GetAxis("Vertical");
 
             Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+            movement.Normalize();
 
             if (movement == Vector3.zero)
             {
@@ -33,7 +34,14 @@ namespace PlayerOne
             }
 
             playerRigidbody.MovePosition(playerRigidbody.position + movement * Time.fixedDeltaTime);
+            transform.Translate(movement * dSpeed * Time.fixedDeltaTime, Space.World);
+            if (movement != Vector3.zero)
+            {
+                Quaternion rotateQuat = Quaternion.LookRotation(movement,Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateQuat, rotateSpeed * Time.deltaTime);
+            }
         }
+
 
         private void Update()
         {
@@ -45,17 +53,49 @@ namespace PlayerOne
             bool sneaking = playerAnimator.GetBool("Sneaking");
 
 
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKey(KeyCode.W))
             {
                 playerAnimator.SetBool("Walking", true);
             }
+
+            else if (Input.GetKey(KeyCode.A))
+            {
+                playerAnimator.SetBool("Walking", true);
+
+            }
+
+            else if (Input.GetKey(KeyCode.S))
+            {
+                playerAnimator.SetBool("Walking", true);
+
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                playerAnimator.SetBool("Walking", true);
+
+            }
+            else
+            {
+                playerAnimator.SetBool("Walking", false);
+            }
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 playerAnimator.SetBool("Jumping", true);
             }
-            if (Input.GetKeyDown(KeyCode.C))
-            {
+            if (Input.GetKey(KeyCode.C))
+            {   
+                if (walking == true)
+                {
+                    playerAnimator.SetBool("Sneaking", true);
+                    dSpeed = 0.05f;
+                }
 
+            }
+            else
+            {
+                playerAnimator.SetBool("Sneaking", false);
+                dSpeed = 0.175f;
             }
         }
     }

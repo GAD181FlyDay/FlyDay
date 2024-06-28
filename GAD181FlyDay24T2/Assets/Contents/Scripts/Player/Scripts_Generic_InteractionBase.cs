d5478c5624a714c85a2e10f8d71dfd0252c224ca
Exkalybur9134 <kalyamba.mhango@student.saedubai.com>
2024-06-28 16:14:34 +0400

    Fully Implemented Interact
    With base interaction script implemented. Override function for usage on other objects. Finally lmao.

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/Scripts_Generic_InteractionBase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts_Generic_InteractionBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public virtual void Interact()
+    {
+        Debug.Log("Touched lmao");
+    }
+}
--- GAD181FlyDay24T2/Assets/Contents/Scripts/Player/Scripts_Player_GenericInteractions.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/Scripts_Player_GenericInteractions.cs
@@ -11,6 +11,8 @@ public class Scripts_Player_GenericInteractions : MonoBehaviour
     public Transform playerModel;
     public TextMeshProUGUI PressE;
     public float InteractRange;
+    public LayerMask interactibleLayer;
+    private Scripts_Generic_InteractionBase currentInteraction;
     void Awake()
     {
         PressE.gameObject.SetActive(false);
@@ -19,16 +21,22 @@ public class Scripts_Player_GenericInteractions : MonoBehaviour
     private void Update()
     {
         Ray interactRay = new Ray(playerModel.position, playerModel.forward);
-        if(Physics.Raycast (interactRay, out RaycastHit hitInfo, InteractRange))
+        if(Physics.Raycast (interactRay, out RaycastHit hitInfo, InteractRange, interactibleLayer))
         {
-            if (hitInfo.collider.tag == "Interactible")
+            if (hitInfo.collider.CompareTag ("Interactible"))
             {
                 PressE.gameObject.SetActive (true);
+                currentInteraction = hitInfo.collider.GetComponent<Scripts_Generic_InteractionBase>(); ;
             }
         }
         else
         {
             PressE.gameObject.SetActive(false);
+        }
+
+        if (PressE.gameObject.activeSelf && Input.GetKeyDown(KeyCode.E))
+        {
+            currentInteraction?.Interact();
         }
     }
 }

778f0ef8d32b184363ccecea31dc77f6704fdfd9
Dana <tetekim31@gmail.com>
2024-07-04 22:47:08 +0400

    Added an empty script.

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/TaxiMeter/PlayerKeysHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TaxiMingame
+{
+
+    /// <summary>
+    /// Handles keyboard inputs!
+    /// </summary>
+    
+    public class PlayerKeysHandler : MonoBehaviour
+    {
+        
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}

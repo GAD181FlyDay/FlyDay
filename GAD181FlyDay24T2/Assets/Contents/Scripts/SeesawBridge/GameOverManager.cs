a5d881dd3156bfa9b5722fa27eee62ecd3b7cb6d
Dana <tetekim31@gmail.com>
2024-07-07 20:14:29 +0400

    Implemented Time pausing and unpausing in seesawbridge

--- GAD181FlyDay24T2/Assets/Contents/Scripts/SeesawBridge/GameOverManager.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/SeesawBridge/GameOverManager.cs
@@ -8,11 +8,13 @@ public class GameOverManager : MonoBehaviour
     [SerializeField] GameObject gameOverPanel;
     public void ResetScene()
     {
+        Time.timeScale = 1.0f;
         SceneManager.LoadScene("SeesawBridge");
     }
     public void EnableGameOverUI()
     {
         gameOverPanel.SetActive(true);
+        Time.timeScale = 0;
     }
 
 }

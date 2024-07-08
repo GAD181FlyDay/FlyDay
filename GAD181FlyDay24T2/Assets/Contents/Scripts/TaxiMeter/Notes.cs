86384f27d272f9d49285d2b2a695acb089af9647
Dana <tetekim31@gmail.com>
2024-07-08 19:50:56 +0400

    Created a simple Note 'script'
    This will be attached to note prefabs to declare what type of note they are.

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/TaxiMeter/Notes.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace TaxiMeter
+{
+    /// <summary>
+    /// This stores notes/ types of notes.
+    /// </summary>
+    public class Notes : MonoBehaviour
+    {
+        public string noteType; // Type of the note.
+                                // This script is to be attached to different notes to define their type!
+    }
+}

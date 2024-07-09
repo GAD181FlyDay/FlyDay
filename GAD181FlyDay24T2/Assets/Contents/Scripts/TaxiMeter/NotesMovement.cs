6b50cedcce00d0aed715ec90ebbd9da8bd97b4f5
Dana <tetekim31@gmail.com>
2024-07-09 08:32:41 +0400

    created Notes movement logic.
     I still need to refine it through other scripts to have them fall from outside of the screen top. and I also need to test out the bottom screen bound.

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/TaxiMeter/NotesMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TaxiMeter
+{
+    /// <summary>
+    /// This script is responsible for the notes' movement.
+    /// </summary>
+
+    public class NotesMovement : MonoBehaviour
+    {
+        #region Variables
+        public float speed = 5f; // Speed at which the note falls
+        [SerializeField] private float bottomScreenOutboundReachedPosition = -5.5f; // make this a const once I'm sure of the screen outbound.
+        #endregion
+
+        private void Update()
+        {
+            NotesMovementLogic();
+        }
+
+        #region Private Functions
+        private void NotesMovementLogic()
+        {
+            // Move the note downwards.
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+            // Check if the note has exited the screen
+            if (transform.position.y < bottomScreenOutboundReachedPosition)
+            {
+                Destroy(gameObject);
+            }
+        }
+        #endregion
+    }
+}

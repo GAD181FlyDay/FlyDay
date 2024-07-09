9b02d7a20bc99427d292b77f95b7daf3c121d4fa
Dana <tetekim31@gmail.com>
2024-07-09 18:00:33 +0400

    Edited UI script so that the last selected button is The play button
    Faced an issue where when the option or credit pane is on, the players cant exit using keys, instead it plays the game.

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/UI/CustomEventSystemInputSystem.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace EventSystem
+{
+    /// <summary>
+    /// This script is to override a Unity script so that
+    /// when you click outside  a button, the buttons remain
+    /// navigatable through keys.
+    /// </summary>
+
+    public class CustomEventSystemInputSystem : StandaloneInputModule
+    {
+        public override void Process()
+        {
+            base.Process(); 
+
+            // Prevents deselection on background click.
+            if (eventSystem.currentSelectedGameObject == null)
+            {
+                if (lastSelectedGameObject != null)
+                {
+                    eventSystem.SetSelectedGameObject(lastSelectedGameObject);
+                }
+            }
+        }
+
+        private GameObject lastSelectedGameObject;
+
+        public override void UpdateModule()
+        {
+            base.UpdateModule();
+            if (eventSystem.currentSelectedGameObject != null)
+            {
+                lastSelectedGameObject = eventSystem.currentSelectedGameObject;
+            }
+        }
+    }
+}

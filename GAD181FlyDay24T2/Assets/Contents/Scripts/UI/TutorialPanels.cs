5f7b02de198554516cdfd7d2fb52ae8193f02ea7
Dana <tetekim31@gmail.com>
2024-07-11 15:15:12 +0400

    Created a reusable Tutorial panel script for all tutorial panels

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/UI/TutorialPanels.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TutorialPanels : MonoBehaviour
+{
+    /// <summary>
+    /// Attach this script to your tutorial panels!
+    /// At the start of the minigame it pauses time.
+    /// Once the player presses the button, The time is resumed and
+    /// the panel is turned off!
+    /// </summary>
+
+    #region Variables
+    [SerializeField] private GameObject tutorialPanel;
+    [SerializeField] private GameObject[] textsThatAreHidden;
+    #endregion
+
+    private void Update()
+    {
+        OnSpaceTurnOffThePanel();
+    }
+
+    #region Private Functions.
+    private void OnSpaceTurnOffThePanel()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            tutorialPanel.SetActive(false);
+
+            for (int i = 0; i < textsThatAreHidden.Length; i++) 
+            {
+                textsThatAreHidden[i].SetActive(true);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+    #endregion
+
+}

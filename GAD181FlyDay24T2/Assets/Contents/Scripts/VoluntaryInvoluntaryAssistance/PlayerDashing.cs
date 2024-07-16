9e1536c5c9f5eeee18fe913feb48aacfb5a3db6a
Dana <tetekim31@gmail.com>
2024-07-16 21:07:07 +0400

    Setting up the scene with prefabs, Adjusted scripts to suit 3D instead of 2D
    Removed movement because I realised I could re use normal movement

--- GAD181FlyDay24T2/Assets/Contents/Scripts/VoluntaryInvoluntaryAssistance/PlayerDashing.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/VoluntaryInvoluntaryAssistance/PlayerDashing.cs
@@ -9,18 +9,11 @@ namespace VoluntaryInvoluntaryAssistance
     public class PlayerDashing : MonoBehaviour
     {
         #region Variables.
-        public float speed = 5f;
-        public float dashSpeed = 10f;
+        public float dashSpeed = 2f;
         public float dashDuration = 0.5f;
         public Players player;
 
-        private KeyCode _moveLeftKey;
-        private KeyCode _moveRightKey;
-        private KeyCode _moveUpKey;
-        private KeyCode _moveDownKey;
         private KeyCode _dashKey;
-
-
         private float _dashTime = 0f;
         private bool _isDashing = false;
         #endregion
@@ -30,17 +23,9 @@ namespace VoluntaryInvoluntaryAssistance
             switch (player)
             {
                 case Players.one:
-                    _moveLeftKey = KeyCode.A;
-                    _moveRightKey = KeyCode.D;
-                    _moveUpKey = KeyCode.W;
-                    _moveDownKey = KeyCode.S;
                     _dashKey = KeyCode.Q;
                     break;
                 case Players.two:
-                    _moveLeftKey = KeyCode.LeftArrow;
-                    _moveRightKey = KeyCode.RightArrow;
-                    _moveUpKey = KeyCode.UpArrow;
-                    _moveDownKey = KeyCode.DownArrow;
                     _dashKey = KeyCode.RightControl;
                     break;
             }
@@ -48,32 +33,11 @@ namespace VoluntaryInvoluntaryAssistance
 
         private void Update()
         {
-            HandleMovement();
             HandleDash();
         }
 
         #region Private Functions.
-        private void HandleMovement()
-        {
-            if (!_isDashing)
-            {
-                float moveX = 0f;
-                float moveY = 0f;
-
-                if (Input.GetKey(_moveLeftKey))
-                    moveX = -1f;
-                if (Input.GetKey(_moveRightKey))
-                    moveX = 1f;
-                if (Input.GetKey(_moveUpKey))
-                    moveY = 1f;
-                if (Input.GetKey(_moveDownKey))
-                    moveY = -1f;
-
-                Vector2 movement = new Vector2(moveX, moveY).normalized * speed * Time.deltaTime;
-                transform.Translate(movement);
-            }
-        }
-
+        
         private void HandleDash()
         {
             if (Input.GetKeyDown(_dashKey) && !_isDashing)
@@ -85,7 +49,7 @@ namespace VoluntaryInvoluntaryAssistance
             if (_isDashing)
             {
                 _dashTime -= Time.deltaTime;
-                transform.Translate(Vector2.up * dashSpeed * Time.deltaTime);
+                transform.Translate(Vector3.forward * dashSpeed * Time.deltaTime);
 
                 if (_dashTime <= 0)
                 {

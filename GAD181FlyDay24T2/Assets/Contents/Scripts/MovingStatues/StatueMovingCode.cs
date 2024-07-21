9ef5d70b30e32569a7f8d73bc07e10093bc1d322
Tsabit Haqqi <t.haqqi@student.saedubai.com>
2024-07-21 20:12:19 +0400

    Edited collisision detection on script

--- GAD181FlyDay24T2/Assets/Contents/Scripts/MovingStatues/StatueMovingCode.cs
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/MovingStatues/StatueMovingCode.cs
@@ -5,17 +5,42 @@ using UnityEngine;
 public class StatueMovingCode : MonoBehaviour
 {
     public int currentCodeRotation = 0;
+    bool playerDettected;
 
     void RotateStatue()
     {
         transform.Rotate(new Vector3(0, 90, 0)); 
+        if (currentCodeRotation < 3)
+        {
+            currentCodeRotation ++;
+        }
+        else 
+        {
+            currentCodeRotation = 0;
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && playerDettected == true)
         {
             RotateStatue();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerDettected = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerDettected = false;
         }
     }
 }

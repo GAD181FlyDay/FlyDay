c1948d71484328b62400ed008f3fb0abc25bb7d6
Exkalybur9134 <kalyamba.mhango@student.saedubai.com>
2024-07-24 14:34:51 +0400

    Added save data functionality
    No longer using Json files and now saving as a unity local engine file

---
+++ GAD181FlyDay24T2/Assets/Contents/Scripts/Player/PlayerSaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSaveData : MonoBehaviour
+{
+    public int money;
+    public string currentScene;
+    public DialogueStage currentDialogueStage;
+    public Vector3 position; 
+}

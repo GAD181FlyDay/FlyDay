13803d104b4e5bec1347d521d54601f1674ce8e5
Dana <tetekim31@gmail.com>
2024-08-05 19:17:20 +0400

    Deleted some scripts

--- GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/Player/MinigameInteractions/Scripts_Minigame_XnOs.cs
+++ GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/Player/MinigameInteractions/Scripts_Minigame_XnOs.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Scripts_Minigame_JUstifiableShoplifting : Scripts_InteractionBaseToOverride
+public class Scripts_Minigame_JustifiableShoplifting : Scripts_InteractionBaseToOverride
 {
     public override void Interact()
     {

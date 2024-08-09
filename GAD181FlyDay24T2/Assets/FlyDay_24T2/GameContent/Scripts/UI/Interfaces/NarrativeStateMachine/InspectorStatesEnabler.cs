5ef75fad2c9357c24fe9ecc68d4072e224585327
Dana <tetekim31@gmail.com>
2024-08-09 18:28:56 +0400

    Created a narrative stateSwitcher. (careful with using it because you need to turn off and on bools carefully)

---
+++ GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/UI/Interfaces/NarrativeStateMachine/InspectorStatesEnabler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class InspectorStatesEnabler : MonoBehaviour
+{
+    #region Variables.
+    public bool firstState;
+    public bool secondState;
+    public bool thirdState;
+    public bool fourthState;
+    public bool fifthState;
+    public bool sixthState;
+    public bool seventhState;
+    public bool eighthState;
+    public bool ninthState;
+
+    [SerializeField] private PlayerSaveData playerSaveData;
+    #endregion
+    
+    private void Update()
+    {
+        if (firstState) { playerSaveData.currentStateInt = 0; }
+        if (secondState) { playerSaveData.currentStateInt = 1; }
+        if (thirdState) { playerSaveData.currentStateInt = 2; }
+        if (fourthState) { playerSaveData.currentStateInt = 3; }
+        if (fifthState) { playerSaveData.currentStateInt = 4; }
+        if (sixthState) { playerSaveData.currentStateInt = 5; }
+        if (seventhState) { playerSaveData.currentStateInt = 6;}
+        if (eighthState) { playerSaveData.currentStateInt= 7;}
+        if (ninthState) { playerSaveData.currentStateInt= 8;}
+    
+    }
+}

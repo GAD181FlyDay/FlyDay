5d28666fac1667e3c6a0600b32540d6329c00bdf
Dana <tetekim31@gmail.com>
2024-08-09 18:10:15 +0400

    Currency displays in Main Game Scene

--- GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/Scene/MiniGameStandsAndCurrencySceneManager.cs
+++ GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/Scene/MiniGameStandsAndCurrencySceneManager.cs
@@ -1,16 +1,21 @@
-
-
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // Hello!! Attach to any game object within main game scene!!!
 
-public class TEMP_SceneManager : MonoBehaviour
+public class MiniGameStandsAndCurrencySceneManager : MonoBehaviour
 {
+    #region Variables.
     public string sceneToAlwaysLoad = "MinigameStands"; 
+    public PlayerSaveData playerSaveData;
 
-    void Start()
+    [SerializeField] private TMP_Text currencyText;
+    #endregion
+
+    private void Start()
     {
+        currencyText.text = playerSaveData.mainLuckyCoinsSource + "";
         if (!SceneManager.GetSceneByName(sceneToAlwaysLoad).isLoaded)
         {
             SceneManager.LoadScene("MinigameStands", LoadSceneMode.Additive);

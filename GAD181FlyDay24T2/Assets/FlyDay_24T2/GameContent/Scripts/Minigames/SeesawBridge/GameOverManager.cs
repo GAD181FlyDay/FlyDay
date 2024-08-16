c1307cc7ea779dee3427489777993e63949a1922
Dana <tetekim31@gmail.com>
2024-08-16 12:24:26 +0400

    Coloured the taxi stand

--- GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/Minigames/SeesawBridge/GameOverManager.cs
+++ GAD181FlyDay24T2/Assets/FlyDay_24T2/GameContent/Scripts/Minigames/SeesawBridge/GameOverManager.cs
@@ -19,6 +19,8 @@ public class GameOverManager : MonoBehaviour
 
     public void EnableGameOverUI()
     {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         gameOverPanel.SetActive(true);
         Time.timeScale = 0;
     }
